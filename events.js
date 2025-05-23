5cfef4c7a9185e4084aaf7a0ccbaae2edde891c3
AolinGuo <philipethanzg@gmail.com>
2025-05-23 09:59:45 +0800

    Add files via upload

---
+++ events.js
@@ -0,0 +1,20 @@
+const events = [
+  {
+    time: new Date("2000-01-01"),
+    title: "出生",
+    description: "出生于中国北京。",
+    image: "birth.jpg"
+  },
+  {
+    time: new Date("2006-09-01"),
+    title: "小学",
+    description: "开始小学学习。",
+    image: "school.jpg"
+  },
+  {
+    time: new Date("2012-06-30"),
+    title: "毕业",
+    description: "小学毕业典礼视频。",
+    video: "graduation.mp4"
+  }
+];
